ad6842c3d0307de2265e873223114e055d5dabe0
alves-Moises <moisesinho_ra@hotmail.com>
2022-08-24 22:43:30 -0300

    update view

--- sessao_03/Django1/core/views.py
+++ sessao_03/Django1/core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "curso": "CURSO DE SEXO GRátis",
-        "outro":  "Essa é a porra da variavel OUTRO< CARALHO, olha ali embaixo a variavel context sendo passada"
+        "curso": "programaçãop web com django framework",
+        "outro":  "Django Django"
     }
     return render(request, 'index.html', context)
 
