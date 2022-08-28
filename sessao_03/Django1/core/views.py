526c503450cdc2222cf55bf5429b68afb3981fb8
alves-Moises <moisesinho_ra@hotmail.com>
2022-08-28 15:19:08 -0300

    update views: index route parameters, new produto

--- sessao_03/Django1/core/views.py
+++ sessao_03/Django1/core/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
+    produtos = Produto.objects.all()
+
+
     context = {
         "curso": "programaçãop web com django framework",
-        "outro":  "Django Django"
+        "outro":  "Django Django",
+        "produtos": produtos
     }
+
+
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
